a7ba7674dc4b01c5dc16bf0f46c477ba2356d5ab
Aslem-Slama <amraslamslama@gmail.com>
2026-01-06 18:29:11 +0100

    .

--- backend/main.py
+++ backend/main.py
@@ -17,3 +17,21 @@ app =FastAPI()
 origins = [
     "http://localhost:3000"
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+
+
+memory_db = {"fruits": []}
+
+@app.get("/fruits", response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits = memory_db.get("fruits"))
+
+
