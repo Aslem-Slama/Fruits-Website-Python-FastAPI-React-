01b1ae031c119b039e1059ad4d16a948d427a4dc
Aslem-Slama <amraslamslama@gmail.com>
2026-01-06 23:57:32 +0100

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import './App.css';
 import FruitList from './components/Fruits';
+import fruitsBg from "./assets/fruits_background.jpg";
+
 
 const App = () => {
   return (
-  <div className="container">
-    <div className="card">
-      <header>
-        <h1>Fruit Management App</h1>
-      </header>
+    <div className="page">
+      <div className="left">
+        <div className="card">
+          <header className="header">
+            <h1>Fruit Management App</h1>
+          </header>
+          <main>
+            <FruitList />
+          </main>
+        </div>
+      </div>
 
-      <main>
-        <FruitList />
-      </main>
+      <div className="right">
+        <div className="imageCard">
+          <img
+            className="heroImage"
+            src={fruitsBg}
+            alt="Fruits"
+            />
+          <div className="imageOverlay">
+            <h2>Track what you have</h2>
+            <p>Add and remove fruit weights in kg.</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default App;
